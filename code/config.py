d29c23458d00ebc4ff3c158cc08164d46a66c21a
Eric Guo <guoanjie@gmail.com>
2018-03-21 22:16:26 -0400

    decode root dir

--- code/config.py
+++ code/config.py
@@ -6,6 +6,6 @@ import subprocess
 competition = 'competitive-data-science-predict-future-sales'
 
 # ------------------------ PATH ------------------------
-ROOT_DIR = subprocess.check_output(['git', 'rev-parse', '--show-toplevel']).rstrip()
+ROOT_DIR = subprocess.check_output(['git', 'rev-parse', '--show-toplevel']).rstrip().decode('utf-8')
 
 DATA_DIR = os.path.join('~/.kaggle/competitions', competition)
