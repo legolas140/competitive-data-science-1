55e3eb9cb5843de2c818694762ca93f30031a8bb
Eric Guo <guoanjie@gmail.com>
2018-03-23 06:48:08 -0400

    install more packages

--- ec2/setup.sh
+++ ec2/setup.sh
@@ -1,5 +1,6 @@
 ROOT_DIR="$(git rev-parse --show-toplevel)"
 
+pip install -U catboost
 pip install -U ipython
 pip install -U jupyter
 pip install -U kaggle
@@ -9,11 +10,12 @@ pip install -U matplotlib
 pip install -U nltk
 pip install -U numpy
 pip install -U pandas
+pip install -U pytorch
 pip install -U matplotlib
 pip install -U seaborn
 pip install -U sklearn
 pip install -U tensorflow
-# pip install -U vowpalwabbit
+pip install -U html5lib
 # pip install -U xgboost
 
 python -c "import nltk; nltk.download('all')"
