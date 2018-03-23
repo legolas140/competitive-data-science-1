3c65df52223dbb2ba5af98120edf671b6fd6e9c2
Eric Guo <guoanjie@gmail.com>
2018-03-22 23:16:27 -0400

    auto-install packages

--- ec2/setup.sh
+++ ec2/setup.sh
@@ -1,6 +1,22 @@
 ROOT_DIR="$(git rev-parse --show-toplevel)"
 
-pip install kaggle
+pip install -U ipython
+pip install -U jupyter
+pip install -U kaggle
+pip install -U keras
+pip install -U lightgbm
+pip install -U matplotlib
+pip install -U nltk
+pip install -U numpy
+pip install -U pandas
+pip install -U matplotlib
+pip install -U seaborn
+pip install -U sklearn
+pip install -U tensorflow
+# pip install -U vowpalwabbit
+# pip install -U xgboost
+
+python -c "import nltk; nltk.download('all')"
 
 mkdir -p $HOME/.kaggle
 cp $ROOT_DIR/ec2/kaggle.json $HOME/.kaggle
