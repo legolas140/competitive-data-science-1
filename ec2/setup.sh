806b7a83928044c5179485c6b1da2818ee776db5
Eric Guo <guoanjie@gmail.com>
2018-03-23 07:44:04 -0400

    install more packages

--- ec2/setup.sh
+++ ec2/setup.sh
@@ -10,16 +10,19 @@ pip install -U matplotlib
 pip install -U nltk
 pip install -U numpy
 pip install -U pandas
-pip install -U pytorch
 pip install -U matplotlib
 pip install -U seaborn
 pip install -U sklearn
 pip install -U tensorflow
 pip install -U html5lib
-# pip install -U xgboost
+pip install -U xgboost==0.6a2
+
+pip3 install http://download.pytorch.org/whl/torch-0.3.1-cp36-cp36m-macosx_10_7_x86_64.whl 
+pip3 install torchvision 
+# macOS Binaries dont support CUDA, install from source if CUDA is needed
 
 python -c "import nltk; nltk.download('all')"
 
 mkdir -p $HOME/.kaggle
 cp $ROOT_DIR/ec2/kaggle.json $HOME/.kaggle
-kaggle competitions download -c competitive-data-science-predict-future-sales
+# kaggle competitions download -c competitive-data-science-predict-future-sales
