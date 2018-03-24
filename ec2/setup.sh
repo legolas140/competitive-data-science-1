744527a3ce02a4c23ef413305f31c4468f5726b4
Eric Guo <guoanjie@gmail.com>
2018-03-24 17:02:48 -0400

    install more packages

--- ec2/setup.sh
+++ ec2/setup.sh
@@ -1,16 +1,20 @@
 ROOT_DIR="$(git rev-parse --show-toplevel)"
 
+pip install -U bokeh
 pip install -U catboost
+pip install -U ggplot
 pip install -U ipython
 pip install -U jupyter
 pip install -U kaggle
 pip install -U keras
 pip install -U lightgbm
 pip install -U matplotlib
+pip install -U networkx
 pip install -U nltk
 pip install -U numpy
 pip install -U pandas
 pip install -U matplotlib
+pip install -U plotly
 pip install -U seaborn
 pip install -U sklearn
 pip install -U tensorflow
@@ -25,4 +29,4 @@ python -c "import nltk; nltk.download('all')"
 
 mkdir -p $HOME/.kaggle
 cp $ROOT_DIR/ec2/kaggle.json $HOME/.kaggle
-# kaggle competitions download -c competitive-data-science-predict-future-sales
+kaggle competitions download -c competitive-data-science-predict-future-sales
